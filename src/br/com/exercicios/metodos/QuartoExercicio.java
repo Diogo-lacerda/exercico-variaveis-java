77afc113d919ee008388c938b417d1764b4c73bb
Diogo Lacerda <dyogot30@gmail.com>
2022-01-11 21:39:43 -0200

    Atualizar o quarto exercício de métodos

--- src/br/com/exercicios/metodos/QuartoExercicio.java
+++ src/br/com/exercicios/metodos/QuartoExercicio.java
@@ -2,26 +2,29 @@ package br.com.exercicios.metodos;
 
 public class QuartoExercicio {
     public static void main(String[] args) {
+        double valorArea;
 
-        calcularArea(3.4, 5.3, 2.1);
-        calcularArea(4.5, 3.5);
-        calcularArea(2.5);
+        valorArea =  calcularArea(3.4, 5.3, 2.1);
+        System.out.println("O valor da área e " + valorArea);
+        valorArea = calcularArea(4.5, 3.5);
+        System.out.println("O valor da área e " + valorArea);
+        valorArea = calcularArea(2.5);
+        System.out.println("O valor da área e " + valorArea);
     }
 
-    private static void calcularArea(double base, double altura, double baseSuperior) {
+    private static double calcularArea(double base, double altura, double baseSuperior) {
         double valorArea = ((base + baseSuperior) * altura) / 2;
-        System.out.println("O valor da área e " + valorArea);
+        return valorArea;
     }
 
-    private static void calcularArea(double base, double altura) {
+    private static double calcularArea(double base, double altura) {
         double valorArea = base * altura;
-        System.out.println("O valor da área e " + valorArea);
+        return valorArea;
 
     }
 
-    private static void calcularArea(double valorLado) {
+    private static double calcularArea(double valorLado) {
         double valorArea = valorLado * valorLado;
-        System.out.println("O valor da área e " + valorArea);
-
+        return valorArea;
     }
 }
