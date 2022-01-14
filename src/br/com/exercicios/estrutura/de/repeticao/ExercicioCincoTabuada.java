962c58a1ce040405441c1190c1b8b1c7a66ab03f
Diogo Lacerda <dyogot30@gmail.com>
2022-01-14 15:27:09 -0200

    Adicionar Quarto exercício de estrutura de repetição

---
+++ src/br/com/exercicios/estrutura/de/repeticao/ExercicioCincoTabuada.java
@@ -0,0 +1,30 @@
+package br.com.exercicios.estrutura.de.repeticao;
+
+import java.util.Scanner;
+
+/*
+Desenvolva um gerador de tabuada, capaz de gerar
+a tabuada de qualquer número inteiro entre 1 a 10.
+O usuário deve informa de qual número ele deseja ver a tabuada.
+A saida deve ser conforme o exemplo abaixo:
+
+Tabuada de 5:
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50
+ */
+public class ExercicioCincoTabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada: ");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada de: " + tabuada);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(tabuada + " x " + i + " = " + (tabuada * i));
+        }
+    }
+
+}
