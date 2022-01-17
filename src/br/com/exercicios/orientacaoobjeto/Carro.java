cc2fe9a816d1d0f3bbb117f8ed97010075e51a1a
Diogo Lacerda <dyogot30@gmail.com>
2022-01-17 18:54:48 -0200

    Adicionar Exercicio Orientação ao Objeto

---
+++ src/br/com/exercicios/orientacaoobjeto/Carro.java
@@ -0,0 +1,48 @@
+package br.com.exercicios.orientacaoobjeto;
+
+public class Carro {
+
+    String cor;                     // Atributos criados para a classe Carro
+    String modelo;                  // Lembrando sempre que tipo de dado que esta sendo usado
+    int capacidadeTanque;
+    //Boas práticas o construtor deve sempre esta abaixo dos atributos
+    //nesta operações abaixo foram feitas as sobrecargas do construtor Carro
+
+    Carro(){
+
+    }
+    Carro( String cor, String modelo, int capacidadeTanque  ){
+        this.cor =cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+
+    }
+
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+    // set conceito usado para guarda algo
+    String getCor() {
+        return cor;
+    }
+    // get conceito usado para busca algo
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    double totalValorTanque(double valorConbustivel){
+        return capacidadeTanque * valorConbustivel;
+    }
+}
