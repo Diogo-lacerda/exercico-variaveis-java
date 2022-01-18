8d25ea73dea2597edf7661f4c5bac7ce7db894a5
Diogo Lacerda <dyogot30@gmail.com>
2022-01-18 02:30:51 -0200

    Adicionar Exercício operação matemática, Orientação ao Objeto

---
+++ src/br/com/exercicios/orientacaoobjeto/interfaces/OperacaoMatematica.java
@@ -0,0 +1,15 @@
+package br.com.exercicios.orientacaoobjeto.interfaces;
+
+public interface OperacaoMatematica {
+
+    double adicao(double valor1, double valor2);
+
+    double subtracao(double valor1, double valor2);
+
+    double multiplicacao(double valor1, double valor2);
+
+    double divisao(double valor1, double valor2);
+
+
+
+}
