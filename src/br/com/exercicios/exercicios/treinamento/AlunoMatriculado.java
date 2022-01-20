657e4fd719b99c3246d135b82b56d5a9e491ce25
Diogo Lacerda <dyogot30@gmail.com>
2022-01-20 13:18:52 -0200

    Refatora Exercício de treinamento

--- src/br/com/exercicios/exercicios/treinamento/AlunoMatriculado.java
+++ src/br/com/exercicios/exercicios/treinamento/AlunoMatriculado.java
@@ -11,14 +11,59 @@ media calcula a média final do aluno (cada prova tem peso 2,5 e o trabalho tem 
 final calcula quanto o aluno precisa para a prova final, (retorna zero se ele não for para a final)
  */
 public class AlunoMatriculado {
-    String nome;
-    String matricula;
-    String idade;
-    double prova1;
-    double prova2;
-    double trabalho;
+    private String nome;
+    private String matricula;
+    private int idade;
+    private double prova1;
+    private double prova2;
+    private double trabalho;
 
-    public double media (){
+    public String getNome(){
+        return nome;
+    }
+    public void setNome (String nome){
+      this.nome = nome;
+    }
+    public double getProva1(){
+        return prova1;
+    }
+    public void setProva1(double prova1){
+        if ( prova1 >= 0 && prova1 <= 10){
+            this.prova1 = prova1;
+        }
+    }
+    public String getMatricula(){
+        return matricula;
+    }
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+    public double getProva2(){
+        return prova2;
+    }
+    public void setProva2(double prova2){
+        if ( prova2 >= 0 && prova2 <= 10){
+            this.prova2 = prova2;
+        }
+    }
+    public double getTrabalho(){
+        return trabalho;
+    }
+    public void setTrabalho(double trabalho) {
+        if (trabalho >= 0 && trabalho <= 10) {
+            this.trabalho = trabalho;
+        }
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        if (idade >= 1){
+            this.idade = idade;
+        }
+    }
+
+        public double media (){
         return (prova1 + prova2 + trabalho) /3;
     }
     public double notaFinal (){
@@ -31,50 +76,5 @@ public class AlunoMatriculado {
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        String aluno = "Diogo Lacerda";
-        String matricula = "FC20172679";
-        System.out.println("Aluno: " + aluno + "\nMatricula: " + matricula);
-        System.out.println("Boletim do primeiro semestre");
-
-        double prova1 ;
-        System.out.println("Nota 1º Prova: ");
-        prova1 = scan.nextDouble();
-        double prova2;
-        System.out.println("Nota 2º Prova: ");
-        prova2 = scan.nextDouble();
-        double trabalho;
-        System.out.println("Nota Trabalho: ");
-        trabalho = scan.nextDouble();
-        double notafinal = 3;
-
-        double media = (prova1 + prova2 + trabalho) / 2;
-        System.out.println("Média: " + media);
-        if (media >= notafinal){
-            System.out.println("Você passou");
-       }
-       else {
-           System.out.println("Você esta de exame final");
-        }
-
-    }
 
 }
--- src/br/com/exercicios/exercicios/treinamento/Principal.java
+++ src/br/com/exercicios/exercicios/treinamento/Principal.java
@@ -4,44 +4,44 @@ public class Principal {
     public static void main(String[] args) {
 
         AlunoMatriculado diogo = new AlunoMatriculado();
-        diogo.nome = "Diogo Lacerda";
-        diogo.matricula = "st54622";
-        diogo.idade = "28";
-        diogo.prova1 = 7;
-        diogo.prova2 = 6;
-        diogo.trabalho = 4;
+        diogo.setNome("Diogo Lacerda");
+        diogo.setMatricula("st54622");
+        diogo.setIdade(28);
+        diogo.setProva1(10);
+        diogo.setProva2(6);
+        diogo.setTrabalho(4);
         diogo.media();
         diogo.notaFinal();
 
 
-        System.out.println("Nome: " + diogo.nome);
-        System.out.println("Matricula: " + diogo.matricula);
-        System.out.println("Idade: " + diogo.idade);
-        System.out.println("Prova 1: " + diogo.prova1);
-        System.out.println("Prova 2: " + diogo.prova2);
-        System.out.println("Trabalho: " + diogo.trabalho );
+        System.out.println("Nome: " + diogo.getNome());
+        System.out.println("Matricula: " + diogo.getMatricula());
+        System.out.println("Idade: " + diogo.getIdade());
+        System.out.println("Prova 1: " + diogo.getProva1());
+        System.out.println("Prova 2: " + diogo.getProva2());
+        System.out.println("Trabalho: " + diogo.getTrabalho() );
         System.out.printf("Média: %.2f\n" , diogo.media() );
         System.out.printf("Restante da nota para o exame final: %.2f\n\n" , diogo.notaFinal());
 
 
 
         AlunoMatriculado victtor = new AlunoMatriculado();
-        victtor.nome = "Victtor";
-        victtor.matricula = "st54822";
-        victtor.idade = "25";
-        victtor.prova1 = 6;
-        victtor.prova2 = 6;
-        victtor.trabalho = 3;
+        victtor.setNome("Victtor");
+        victtor.setMatricula("st54822");
+        victtor.setIdade(25);
+        victtor.setProva1(8);
+        victtor.setProva2(9);
+        victtor.setTrabalho(6);
         victtor.media();
         victtor.notaFinal();
 
 
-        System.out.println("Nome: " + victtor.nome);
-        System.out.println("Matricula: " + victtor.matricula);
-        System.out.println("Idade: " + victtor.idade);
-        System.out.println("Prova 1: " + victtor.prova1);
-        System.out.println("Prova 2: " + victtor.prova2);
-        System.out.println("Trabalho: " + victtor.trabalho);
+        System.out.println("Nome: " + victtor.getNome());
+        System.out.println("Matricula: " + victtor.getMatricula());
+        System.out.println("Idade: " + victtor.getIdade());
+        System.out.println("Prova 1: " + victtor.getProva1());
+        System.out.println("Prova 2: " + victtor.getProva2());
+        System.out.println("Trabalho: " + victtor.getTrabalho());
         System.out.printf("Média: %.2f\n" , victtor.media());
         System.out.printf("Restante da nota para o exame final: %.2f\n " , victtor.notaFinal());
 
